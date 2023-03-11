818bed4829c8f6543a23075fbf109a07f91607fe
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 10:17:39 +0330

    Function: hasTrack

--- ozans-playlist.js
+++ ozans-playlist.js
@@ -11,3 +11,17 @@ function removeDuplicates(playlist) {
 }
 
 // console.log(removeDuplicates(playlist));
+
+///---///
+
+// const playlist = [
+//   "The Fashion Show - Grace Jones",
+//   "Dr. Funkenstein - Parliament"
+// ];
+
+function hasTrack(playlist, track) {
+  return new Set(playlist).has(track);
+}
+
+// console.log(hasTrack(playlist, "Dr. Funkenstein - Parliament"));
+// console.log(hasTrack(playlist, "Walking in the Rain - Grace Jones"));
