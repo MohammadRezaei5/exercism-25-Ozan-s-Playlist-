9aa9bd635c3a5b2b025347b7d7df86405e614b40
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 10:17:15 +0330

    Function: removeDuplicates

--- ozans-playlist.js
+++ ozans-playlist.js
@@ -1,1 +1,13 @@
 // look back, read more ...
+
+// const playlist = [
+//   "Court and Spark - Joni Mitchell",
+//   "Big Yellow Taxi - Joni Mitchell",
+//   "Court and Spark - Joni Mitchell"
+// ];
+
+function removeDuplicates(playlist) {
+  return Array.from(new Set(playlist));
+}
+
+// console.log(removeDuplicates(playlist));
